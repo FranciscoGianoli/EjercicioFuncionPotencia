3ab91be36701abb3ea03fb34feb432bea976e80d
FranciscoGianoli <102192283+FranciscoGianoli@users.noreply.github.com>
2022-04-21 22:01:01 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+let base = <HTMLInputElement>document.getElementById("valor1");
+let exponente = <HTMLInputElement>document.getElementById("valor2");
+let resultado = <HTMLElement>document.getElementById("btnEnviar");
+
+let valorBase: number = Number(base.value);
+let valorExponente: number = Number(exponente.value);
+let potencia: number = Math.pow();
+
+function realizarPotencia() {
+  let valorBase: number = Number("");
+  let valorExponente: number = Number("");
+  console.log(potencia);
+}
